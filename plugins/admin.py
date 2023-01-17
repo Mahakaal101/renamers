9a1e0cddde20ffb7076901822424cbc7b668cbaf
Ezee19 <sonukumarkrbbu67@gmail.com>
2023-01-18 03:51:15 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -4,7 +4,7 @@ import os
 from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit, usertype, addpre
-ADMIN = int(os.environ.get("ADMIN", 1484670284))
+ADMIN = int(os.environ.get("ADMIN", ))
 log_channel = int(os.environ.get("LOG_CHANNEL", ""))
 
 
@@ -123,9 +123,9 @@ async def cp3(bot,update):
 async def dft(bot,update):
 	id = update.message.reply_to_message.text.split("/resetpower")
 	user_id = id[1].replace(" ", "")
-	inlimit = 1500000000
-	uploadlimit(int(user_id), 1500000000)
+	inlimit = 3145728000
+	uploadlimit(int(user_id), 3145728000)
 	usertype(int(user_id),"**Free**")
 	addpre(int(user_id))
-	await update.message.edit("Daily Data limit has been reset successsfully.\nThis account has default 1.5 GB renaming capacity ")
+	await update.message.edit("Daily Data limit has been reset successsfully.\nThis account has default 3.0 GB renaming capacity ")
 	await bot.send_message(user_id,"Your Daily Data limit has been reset successsfully.\n\nCheck your plan here - /myplan\n- Contact Admin 🦋<a href='https://t.me/ipapcorn_helper'>**Developer**</a>🦋")
