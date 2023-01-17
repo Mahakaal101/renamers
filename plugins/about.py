ec4889aceaba3e91c48cb1f2f2af956d6cdc09ff
Ezee19 <sonukumarkrbbu67@gmail.com>
2023-01-18 03:42:51 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -12,4 +12,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"REMAER BOT :- <a href='https://t.me/file2link4u_bot'>KING</a>\nCreater :- <a href='https://t.me/ipapcorn_helper'>🦋Developer🦋</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- VPS\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you **<a href='https://t.me/ipapcorn_helper'>Developerr</a>** for your hard work \n\n❤️ we love you <a href='https://t.me/ipapcorn_helper'>**Developer**</a> ❤️",quote=True)
+	await message.reply_text(f"REMAER BOT :- <a href='https://t.me/tlgrenamerbot'>KING</a>\nCreater :- <a href='https://t.me/ipapcorn_helper'>🦋Developer🦋</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- PAID VPS\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you **<a href='https://t.me/ipapcorn_helper'>Developerr</a>** for your hard work \n\n❤️ we love you <a href='https://t.me/ipapcorn_helper'>**Developer**</a> ❤️",quote=True)
