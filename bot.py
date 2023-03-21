41d5048eb7a4fd29c3afdbf18329137d890911ad
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-03-21 22:47:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,13 @@ API_HASH = os.environ.get("API_HASH", "016d5e115a06ddfb6121823d72ae4d8c")
 
 STRING = os.environ.get("STRING", "")
 
+DB_NAME = os.environ.get("DB_NAME", "renamer")
+
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Dipanshu_021:ad8920@cluster0.f7migc1.mongodb.net/?retryWrites=true&w=majority")
+
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "tlgrenamerbot")
+
+CHANNEL = os.environ.get("CHANNEL", "Film_Update_Official")
 
 
 bot = Client(
